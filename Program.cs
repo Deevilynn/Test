2624f848ac34af34e731d738563e2c886db2b262
Evelynn <voooidwaaalker@gmail.com>
2023-01-25 20:04:50 +0300

    added part with creating needed array

--- Program.cs
+++ Program.cs
@@ -4,3 +4,20 @@ var elements = Console.ReadLine();
 char[] separators = new char[] { ' ', ',', '.', ':', ';','|','/','?','!',};
 string[] substring = elements.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
+
+Console.Write("Массив из строк, длина которых меньше либо равна 3 символа: ");
+int count = 1;
+int i = 0;
+string[] NeededArray;
+
+foreach (string ElementsOfArray in substring)
+{
+if (ElementsOfArray.Length <=3)
+{
+NeededArray = new string[count];
+NeededArray[i]=ElementsOfArray;
+Console.Write(NeededArray[i] + " ");
+count ++;
+i++;
+}
+} 
